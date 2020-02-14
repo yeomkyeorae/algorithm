9dff694327c6f6996989ef3d029f3fc05a42b31c
yeomkyeorae <duarufp06@naver.com>
2020-02-15 00:59:24 +0900

    200215 | SWEA_2383_lunch_time

--- study/SWEA_2383_lunch_time.py
+++ study/SWEA_2383_lunch_time.py
@@ -14,82 +14,82 @@ def find(input_list, row):
             stair_value.append(value)
 
 
-def nearest(person_list, stair_list):
-    global stair_value
-
-    where_to_stair = deque()
-    tmp_list = []
-    for p in person_list:
-        dist = 1000
-        candidate_ix = -10
-        for s_ix, s in enumerate(stair_list):
-            tmp_dist = abs(p[0] - s[0]) + abs(p[1] - s[1])
-            if tmp_dist < dist:
-                dist = tmp_dist
-                candidate_ix = s_ix
-            elif tmp_dist == dist:
-                if stair_value[candidate_ix] > stair_value[s_ix]:
-                    candidate_ix = s_ix
-        tmp_list.append(candidate_ix)
-        where_to_stair.append([candidate_ix, dist])
-
-    return where_to_stair
-
-
-def move(where_to_stair):
+def calc(one_list):
+    global person_list
     global stair_list
     global stair_value
-    global person_list
 
-    ANSWER = 0
-    arrived = []
+    person_dist = deque()
+    for ix, stair_ix in enumerate(one_list):
+        person_dist.append([abs(person_list[ix][0] - stair_list[stair_ix][0]) + abs(person_list[ix][1] - stair_list[stair_ix][1]), stair_ix])
+    stair_dict = {
+        0: deque(),
+        1: deque()
+    }
     waitings = deque()
-    ing_dict = {}
-    for i in range(len(stair_value)):
-        ing_dict[i] = deque()
-    while len(arrived) < len(person_list):
-        for one_d in ing_dict.values():
-            for _ in range(len(one_d)):
-                popped = one_d.popleft()
-                popped -= 1
-                if popped == 0:
-                    arrived.append(True)
-                else:
-                    one_d.append(popped)
-
-        for i in range(len(where_to_stair)):
-            popped = where_to_stair.popleft()
-            if popped[1] == 0:
-                waitings.append(popped[0])
-            else:
-                popped[1] -= 1
-                where_to_stair.append(popped)
+    answer = 0
+    flag = False
+    while len(waitings) or len(person_dist) or flag:
+        for values in stair_dict.values():
+            for _ in range(len(values)):
+                p = values.popleft()
+                p -= 1
+                if p != 0:
+                    values.append(p)
 
         for _ in range(len(waitings)):
-            popped = waitings.popleft()
-            if len(ing_dict[popped]) < 3:
-                ing_dict[popped].append(stair_value[popped])
+            p = waitings.popleft()
+            if len(stair_dict[p[1]]) < 3:
+                stair_dict[p[1]].append(stair_value[p[1]])
             else:
-                waitings.append(popped)
+                waitings.append(p)
 
-        if len(arrived) != len(person_list):
-            ANSWER += 1
-    return ANSWER
+        for _ in range(len(person_dist)):
+            p = person_dist.popleft()
+            if p[0] == 0:
+                waitings.append(p)
+            else:
+                p[0] -= 1
+                person_dist.append(p)
+
+        for values in stair_dict.values():
+            if len(values):
+                flag = True
+                break
+            else:
+                flag = False
+
+        answer += 1
+
+    return answer
+
+
+def permutation(stage, one_list, length):
+    global ANSWER
+
+    if stage == length:
+        answer = calc(one_list)
+        if ANSWER > answer:
+            ANSWER = answer
+        return
+
+    for i in range(2):
+        one_list.append(i)
+        permutation(stage + 1, one_list, length)
+        one_list.pop()
 
 
 tries = int(input())
-
 for t in range(1, tries + 1):
     N = int(input())
-    person_list = []
-    stair_list = []
-    stair_value = []
+    person_list = []    # [(row, col), (row, col), ... ]
+    stair_list = []     # [(row, col), (row, col)]
+    stair_value = []    # [3, 5]
 
-    board = []
     for row in range(N):
         tmp_list = list(map(int, input().split()))
         find(tmp_list, row)
-        board.append(tmp_list)
-    where_to_stair = nearest(person_list, stair_list)
-    ANSWER = move(where_to_stair)
-    print('#{} {}'.format(t, ANSWER + 1))
+
+    ANSWER = 1000
+    permutation(0, [], len(person_list))
+    print('#{} {}'.format(t, ANSWER - 1))
