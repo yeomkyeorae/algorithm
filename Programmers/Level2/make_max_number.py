f4dc6ceee89f82cd10d3d53900205b7759f28dc1
yeomkyeorae <duarufp06@naver.com>
2021-10-29 19:20:29 +0900

    programmers: lv2, make_max_number(solved)

--- Programmers/Level2/make_max_number.py
+++ Programmers/Level2/make_max_number.py
@@ -1,22 +1,50 @@
-def solution(number, k):
-    lst = list(map(lambda x: int(x), number))
+# unresolved version
+
+# def solution(number, k):
+#     lst = list(map(lambda x: int(x), number))
     
-    while k > 0:
-        tmp = None
-        flag = False
-        for i, num in enumerate(lst):
+#     while k > 0:
+#         tmp = None
+#         flag = False
+#         for i, num in enumerate(lst):
+#             if k == 0:
+#                 break
+#             if tmp is not None and tmp < num:
+#                 lst.pop(i - 1)
+#                 k -= 1
+#                 flag = True
+#                 break
+#             else:
+#                 tmp = num
+
+#         if not flag:
+#             lst.pop()
+#             k -= 1
+
+#     return ''.join(str(n) for n in lst)
+
+def solution(number, k):
+    new_lst = []
+    lst = list(map(lambda x: int(x), number))
+
+    for num in lst:         
+        if not new_lst:
+            new_lst.append(num)
+            continue
+
+        while new_lst:
             if k == 0:
                 break
-            if tmp is not None and tmp < num:
-                lst.pop(i - 1)
-                k -= 1
-                flag = True
-                break
+            tmp = new_lst.pop()
+            if tmp < num:            
+                k -= 1          
             else:
-                tmp = num
+                new_lst.append(tmp)
+                break
+        new_lst.append(num)
+    
+    if k:
+        for _ in range(k):
+            new_lst.pop()
 
-        if not flag:
-            lst.pop()
-            k -= 1
-
-    return ''.join(str(n) for n in lst)
+    return ''.join(str(n) for n in new_lst)
