4cd86fe089ee5e89eb0beaf9b5ed32e1a1dd2525
yeomkyeorae <duarufp06@naver.com>
2021-12-10 16:28:22 +0900

    BJ, 2xn tiling

---
+++ BJ/DP/BJ_11726_2xn_tile.py
@@ -0,0 +1,20 @@
+import sys
+sys.setrecursionlimit(2000)
+
+
+d = [0] * (1000 + 1)
+
+def dp(n):
+    if n == 1: return 1
+    if n == 2: return 2
+    
+    if d[n] != 0: return d[n]
+    
+    d[n] = (dp(n - 1) + dp(n - 2)) % 10007
+    return d[n]
+
+
+n = int(input())
+result = dp(n)
+
+print(result)
